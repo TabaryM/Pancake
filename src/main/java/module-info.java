c768c423ca00b05ccb53d35913712d6b71ef350b
TabaryM <mathieutab@free.fr>
2021-02-25 14:49:52 +0100

    On retire les éléments de Maven qui ne servent plus

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,5 @@
 // https://edencoding.com/javafx-maven/
 module Pancake {
-    requires javafx.controls;
-    requires javafx.fxml;
-    opens fr.ul.ia to javafx.fxml;
     exports fr.ul.ia;
     exports fr.ul.ia.modele;
     exports fr.ul.ia.exception;
